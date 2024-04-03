af9a7ab4b066474d424df9bb0103b38dc9438ea7
imimran <al.imran@technonext.com>
2024-04-03 15:16:40 +0600

    Nav & Hero section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+
+    </>
+  )
+}
+
+export default App
